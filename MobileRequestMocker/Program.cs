2f0fb69df4db832039930fccf2afbc68b2875e9a
brankomdcs <brankom@microsoft.com>
2021-06-23 15:51:04 +0200

    Adding more options to MobileRequestMocker

--- MobileRequestMocker/Program.cs
+++ MobileRequestMocker/Program.cs
@@ -11,19 +11,19 @@ namespace MobileRequestMocker
 {
     class Program
     {
-        const int numberOfRandomRequestsToSend = 30;
-        const int maxConcurrentRequests = 5;
+        const int numberOfRandomRequestsToSendInOneBatch = 100;
+        const int maxConcurrentRequests = 100;
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Press 's' if you want to skip user registration or any key to start normaly");
-            string enteredOption = Console.ReadLine();
+            Console.WriteLine("Enter 'y' if you would like to register users, enter anything else to skip.");
+            bool registerUsersStepSelected = Console.ReadLine() == "y";
 
             HttpClient httpClient = new HttpClient();
             UserRepository userRepository = new UserRepository();
 
             ServicePointManager.FindServicePoint(new Uri("http://localhost:8080")).ConnectionLimit = maxConcurrentRequests;
 
-            if (enteredOption != "s") {
+            if (registerUsersStepSelected) {
                 RegisterUserRequestGenerator registerUserRequestGenerator = new RegisterUserRequestGenerator();
                 AddCreditRequestGenerator addCreditRequestGenerator = new AddCreditRequestGenerator();
 
@@ -34,21 +34,46 @@ namespace MobileRequestMocker
                 }
             }
 
-            for (int i = 0; i < numberOfRandomRequestsToSend; i++) {
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                Task.Run(async () =>
-                {
-                    Request randomRequest = RandomHttpRequestGenerator.GenerateRandomRequestForTheSet(userRepository.Users);
-                    if (randomRequest.Type == RequestType.HttpGet) {
-                        await httpClient.GetAsync(randomRequest.Url);
-                    } else
-                    {
-                        await httpClient.PostAsync(randomRequest.Url, null);
-                    }
-                });
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            }
+            Console.WriteLine($"Enter 'b' to send random requests in batches or 'l' to send them in infinite loop. Enter anything else to terminate.");
+            string option = Console.ReadLine();
 
+            if (option == "b")
+            {
+                Console.WriteLine($"Enter 'y' to send the batch of {numberOfRandomRequestsToSendInOneBatch} randomly generated requests or anything else to terminate.");
+                while (Console.ReadLine() == "y")
+                {
+                    for (int i = 0; i < numberOfRandomRequestsToSendInOneBatch; i++)
+                    {
+                        SendRandomRequest(httpClient, userRepository);
+                    }
+                    Console.WriteLine($"Enter 'y' to send the batch of {numberOfRandomRequestsToSendInOneBatch} randomly generated requests or anything else to terminate.");
+                }
+            }
+            else if (option == "l") {
+                while (true) {
+                    SendRandomRequest(httpClient, userRepository);
+                }
+            }
+            
+
+        }
+
+        private static void SendRandomRequest(HttpClient httpClient, UserRepository userRepository)
+        {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(async () =>
+            {
+                Request randomRequest = RandomHttpRequestGenerator.GenerateRandomRequestForTheSet(userRepository.Users);
+                if (randomRequest.Type == RequestType.HttpGet)
+                {
+                    await httpClient.GetAsync(randomRequest.Url);
+                }
+                else
+                {
+                    await httpClient.PostAsync(randomRequest.Url, null);
+                }
+            });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
     }
 }
